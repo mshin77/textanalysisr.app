f640c9b5b1ff0ce0d033f67c7a0b5be6b8d2a57e
Mikyung Shin <shin.mikyung@gmail.com>
2023-12-28 11:05:17 -0600

    data

--- server.R
+++ server.R
@@ -15,28 +15,6 @@ suppressPackageStartupMessages({
 })
 
 Sys.setlocale(category = "LC_ALL", locale = "en_US.UTF-8")
-database <- "sped_tech"
-collection <- "text_analysis"
-
-options(mongodb = list(
-  "host" = "cluster0.eyp9hhw.mongodb.net",
-  "username" = "mshin77",
-  "password" = "32hzOLqv42MsMTEn"
-))
-
-loadData <- function() {
-  db <- mongo(collection = collection,
-              url = sprintf(
-                "mongodb+srv://%s:%s@%s/%s",
-                options()$mongodb$username,
-                options()$mongodb$password,
-                options()$mongodb$host,
-                database
-              ),
-              options = ssl_options(weak_cert_validation = TRUE))
-  data <- db$find()
-  data
-}
 
 server <- shinyServer(function(input, output, session) {
   
@@ -50,7 +28,7 @@ server <- shinyServer(function(input, output, session) {
 
     mydata <- reactive({
         if (input$dataset_choice == "SpecialEduTech") {
-            data <- loadData() %>% tibble()
+            data <- SpecialEduTech
         } else {
             req(input$file)
             filename <- input$file$datapath
